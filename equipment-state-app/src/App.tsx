d3791efaa489430b33e7012e911eb6cd233fe8a6
rosenberg71 <peterrosenberg71@gmail.com>
2023-09-21 20:52:20 +0200

    delete autogenerated test files add comment to App.tsx

--- equipment-state-app/src/App.tsx
+++ equipment-state-app/src/App.tsx
@@ -2,10 +2,11 @@ import './App.css';
 import EquipmentComponent from './components/Equipment/Equipment';
 
 function App() {
+  // NOTE: I have added 3 equipment components to simulate multiple clients
   return (
     <div className="App">
       <header className="App-header">
-        <EquipmentComponent />
+        <EquipmentComponent /> 
         <EquipmentComponent />
         <EquipmentComponent />
       </header>
